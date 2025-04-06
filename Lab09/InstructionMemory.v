62028654783ca333501178c0d8973787e737e360
vincentthicc <hi@raspberrypi>
2025-04-06 15:25:31 -0500

    Last push before adding movz functionality (adding 1 bit to <signop>)

--- Lab09/InstructionMemory.v
+++ Lab09/InstructionMemory.v
@@ -62,6 +62,29 @@ module InstructionMemory(Data, Address);
 
 	/* Add code for your tests here */
 
+		/* Test Program 2 (Question 5):
+	 * Program loads constants from the data memory. Uses these constants to test
+	 * the following instructions: LDUR, ORR, AND, CBZ, ADD, SUB, STUR and B.
+	 * 
+	 * Assembly code for test:
+	 * 
+	 * 0: LDUR X9, [XZR, 0x0]    //Load 1 into x9
+	 * 4: LDUR X10, [XZR, 0x8]   //Load a into x10
+	 * 8: LDUR X11, [XZR, 0x10]  //Load 5 into x11
+	 * C: LDUR X12, [XZR, 0x18]  //Load big constant into x12
+	 * 10: LDUR X13, [XZR, 0x20]  //load a 0 into X13
+	 * 
+	 * 14: ORR X10, X10, X11  //Create mask of 0xf
+	 * 18: AND X12, X12, X10  //Mask off low order bits of big constant
+	 * 
+	 * loop:
+	 * 1C: CBZ X12, end  //while X12 is not 0
+	 * 20: ADD X13, X13, X9  //Increment counter in X13
+	 * 24: SUB X12, X12, X9  //Decrement remainder of big constant in X12
+	 * 28: B loop  //Repeat till X12 is 0
+	 * 2C: STUR X13, [XZR, 0x20]  //store back the counter value into the memory location 0x20
+	 */
+
 	
 	default: Data = 32'hXXXXXXXX;
       endcase
